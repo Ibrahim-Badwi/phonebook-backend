20f7510a925b9d8e3752d983faea9a726ca85606
Ibrahim Badwi <ib.a.badwi@gmail.com>
2020-10-24 22:08:12 +0300

    react build files

---
+++ build/precache-manifest.7a3a9d8f3690c808be485b9a8ef52254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d7ff167c2a083789a4116eccd1e775",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe251952462d57c297ab",
+    "url": "/static/js/2.348f4004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.348f4004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f69470ca81a607cab957",
+    "url": "/static/js/main.99e29ddd.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
---
+++ build/precache-manifest.ddd059660b9cb3e2b6cbedfbe9220ca1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a3b3f611a7c8c3ed9a2bf176a8b1d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe251952462d57c297ab",
+    "url": "/static/js/2.348f4004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.348f4004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52fcb772158e7667daba",
+    "url": "/static/js/main.71c70178.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
